54dada21268510fa0f4ec03e6b911319620f2783
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-11-22 19:24:44 +0530

    did configuration using imperative way and declaritive both using labda and normal classes and method

--- src/main/java/com/raghav/spring_security_demo/config/SecurityConfig.java
+++ src/main/java/com/raghav/spring_security_demo/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package com.raghav.spring_security_demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.AuthorizeHttpRequestsDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,11 +17,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+//        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+//                      configurer.disable();
+//            }
+//        };
+//
+//        httpSecurity.csrf(custCsrf);
+//
+//        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> custHttp = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+//            @Override
+//            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+//                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+//            }
+//        };
+//
+//        httpSecurity.authorizeHttpRequests(custHttp);
+
         httpSecurity.csrf(customizer -> customizer.disable());
         httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.formLogin(Customizer.withDefaults());
         httpSecurity.httpBasic(Customizer.withDefaults());
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return httpSecurity.build();
     }
 }
