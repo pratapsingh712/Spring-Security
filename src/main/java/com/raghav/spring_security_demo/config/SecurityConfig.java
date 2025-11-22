1bb2aae394f48dba7a745b3663fcc2be505557e5
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-11-22 19:13:59 +0530

    added spring security, created config for stateless session management

---
+++ src/main/java/com/raghav/spring_security_demo/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.raghav.spring_security_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.csrf(customizer -> customizer.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+    }
+}
